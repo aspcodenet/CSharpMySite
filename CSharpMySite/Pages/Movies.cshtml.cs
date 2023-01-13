bdea44987ac20a32dcd24581d6fa1fd6dbc0b7df
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-01-13 11:14:40 +0100

    added movies

--- CSharpMySite/Pages/Movies.cshtml.cs
+++ CSharpMySite/Pages/Movies.cshtml.cs
@@ -1,3 +1,4 @@
+using CSharpMySite.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,30 @@ namespace CSharpMySite.Pages
 {
     public class MoviesModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
+        public class MovieViewModel
+        {
+            public string Title { get; set; }
+            public string Director { get; set; }
+            public string GenreName { get; set; }
+        }
+
+        public List<MovieViewModel> Items { get; set; }
+
+        public MoviesModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
+            Items = _context.Movies.Select(m => new MovieViewModel
+            {
+                Director = m.Director,
+                GenreName = m.Genre.Name,
+                Title = m.Title,
+            }).ToList();
         }
     }
 }
