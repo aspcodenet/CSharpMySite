0e7a67ad74a34d425445d1891d9c47a5e765d47e
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-01-16 14:02:11 +0100

    aaa

---
+++ CSharpMySite/Services/CanIBuyBeerCalculator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Connections.Features;
+
+namespace CSharpMySite.Services;
+
+
+public enum Location
+{
+    Krogen,
+    Systemet
+}
+public class CanIBuyBeerCalculator
+{
+    public bool CanIBuyBeer(Location location, int age, float promille)
+    {
+        if(location == Location.Krogen && age > 17 && promille < 1.0)
+            return true;
+        if (location == Location.Systemet && age > 17 && promille < 1.0)
+            return true;
+        return false;
+    }
+}
---
+++ CSharpMySiteTests/Services/CanIBuyBeerCalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿using CSharpMySite.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMySiteTests.Services
+{
+    [TestClass]
+    public class CanIBuyBeerCalculatorTests
+    {
+        private CanIBuyBeerCalculator sut; 
+        public CanIBuyBeerCalculatorTests()
+        {
+            sut = new CanIBuyBeerCalculator();
+        }
+
+        [TestMethod]
+        public void When_on_krogen_and_19_but_too_drunk_no_beer_will_be_served()
+        {
+            // Arrange
+            float promille = 2.0f;
+            int age = 19;
+            var location = Location.Krogen;
+
+            // Act
+            var result = sut.CanIBuyBeer(location, age, promille);
+
+            // Assert 
+            Assert.AreEqual(false, result);
+        }
+    }
+
+}
